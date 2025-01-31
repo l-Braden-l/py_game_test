56ab95611b900ce4fca4fa21391988116d01a611
braden <leachbr787@northwested.org>
2025-01-31 13:04:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import pygame
 
+pygame.init()
+
+widow = pygame.display.set_mode((640,640))
+background = pygame.image.load('indie.jpg').convert()
+
+running = True 
+x = 0
+while running:
+
+    widow.blit(background,(x,30))
+
+    x += 1
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+            
+    pygame.display.flip()
+
+pygame.quit()
